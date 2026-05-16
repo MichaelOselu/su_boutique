f830606d8c083de196d89d47b32780ad19015709
MichaelOselu <michaeloselu4@gmail.com>
2026-05-16 16:53:13 +0300

    Add Dockerfile for Render deployment

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * Trust all proxies (Render, Cloudflare, etc.)
+     */
+    protected $proxies = '*';
+
+    /**
+     * Headers used to detect HTTPS properly
+     */
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO;
+}
